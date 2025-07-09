2109237e47e87aae91086e22575f1e0fb3146e33
Ravi Panchal <ravipanchal9977@gmail.com>
2025-07-09 09:13:05 +0530

    feat: add Navbar and Sidebar components for admin panel navigation

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { assets } from "../../assets/assets.js"; // Adjust the import path as necessary
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container flex items-center justify-between px-6 md:px-8 py-2 bg-white shadow">
+      <div className="logo">
+        <h1 className="logo-text text-xl md:text-3xl text-orange-500 font-bold font-quicksand">
+          hungrify
+        </h1>
+        <p className="logo-subtext text-sm text-gray-700">Admin Panel</p>
+      </div>
+      <div className="profile">
+        <div className="image size-8 md:size-11 rounded-full overflow-hidden">
+          <img src={assets.profile_image} alt="Profile" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+import { FaClipboardList } from "react-icons/fa";
+import { IoAddCircle } from "react-icons/io5";
+import { MdViewList } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+import { RiDashboardFill } from "react-icons/ri";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container w-24 md:w-64 min-h-screen bg-white text-slate-800 shadow">
+      <div className="sidebar-options flex flex-col gap-6 pt-8 pl-8">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `sidebar-option group flex items-center gap-4 cursor-pointer py-2 px-2
+     transition-all duration-200 ${
+       isActive ? "bg-icon text-white" : " hover:bg-icon hover:text-white"
+     }`
+          }
+        >
+          <RiDashboardFill
+            size={24}
+            className={`transition duration-200 group-hover:scale-110`}
+          />
+          <span
+            className={`hidden md:inline-block tracking-normal transition-all duration-200 group-hover:tracking-wide`}
+          >
+            Dashboard
+          </span>
+        </NavLink>
+        <NavLink
+          to="/add"
+          className={({ isActive }) =>
+            `sidebar-option group flex items-center gap-4 cursor-pointer py-2 px-2
+     transition-all duration-200 ${
+       isActive ? "bg-icon text-white" : " hover:bg-icon hover:text-white"
+     }`
+          }
+        >
+          <IoAddCircle
+            size={24}
+            className={`transition duration-200 group-hover:scale-110`}
+          />
+          <span
+            className={`hidden md:inline-block tracking-normal transition-all duration-200 group-hover:tracking-wide`}
+          >
+            Add Items
+          </span>
+        </NavLink>
+
+        <NavLink
+          to="/products"
+          className={({ isActive }) =>
+            `sidebar-option group flex items-center gap-4 cursor-pointer py-2 px-2
+     transition-all duration-200 ${
+       isActive ? "bg-icon text-white" : " hover:bg-icon hover:text-white"
+     }`
+          }
+        >
+          <MdViewList
+            size={24}
+            className="transition duration-200 group-hover:scale-110"
+          />
+          <span className="hidden md:inline-block tracking-normal transition-all duration-200 group-hover:tracking-wide">
+            List Items
+          </span>
+        </NavLink>
+
+        <NavLink
+          to="/orders"
+          className={({ isActive }) =>
+            `sidebar-option group flex items-center gap-4 cursor-pointer py-2 px-2
+     transition-all duration-200 ${
+       isActive ? "bg-icon text-white" : " hover:bg-icon hover:text-white"
+     }`
+          }
+        >
+          <FaClipboardList
+            size={24}
+            className="transition duration-200 group-hover:scale-110"
+          />
+          <span className="hidden md:inline-block tracking-normal transition-all duration-200 group-hover:tracking-wide">
+            Orders
+          </span>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
