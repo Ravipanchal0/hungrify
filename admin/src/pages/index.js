bee5775c189e41b008bf96ff3aba09179c4ac848
Ravi Panchal <ravipanchal9977@gmail.com>
2025-07-09 09:13:29 +0530

    feat: add Home, Dashboard, Orders, and Products pages with basic structure

---
+++ admin/src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from "./Dashboard";
+import Orders from "./Orders";
+import Products from "./Products";
+import AddItem from "./AddItem";
+
+export { Dashboard, Orders, Products, AddItem };
