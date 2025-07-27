c285ae0dbe5f29980171b35f3643a4d8b0b523b0
Ravi Panchal <ravipanchal9977@gmail.com>
2025-07-27 23:44:56 +0530

    fix: frontend url

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 export const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET_KEY,
